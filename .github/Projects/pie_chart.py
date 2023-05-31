c24b446de98f7b1c08a0b1ebf8c8550101bbe04f
Farhanrahman063 <123469558+Farhanrahman063@users.noreply.github.com>
2023-05-31 10:25:04 +0600

    Update pie_chart.py

--- .github/Projects/pie_chart.py
+++ .github/Projects/pie_chart.py
@@ -1,7 +1,8 @@
 """System module."""
 
 def pie_chart(degree):
-  print(f"The Pie chart percentage is: {360 * value / degree}")
+    print(f"The Pie chart percentage is: {360 * value / degree}")
+
 
 value = 240
 pie_chart(66)
