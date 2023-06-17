1f364c3802b50dea3bd5c004ac9b2a96b11c176b
Farhanrahman063 <123469558+Farhanrahman063@users.noreply.github.com>
2023-06-17 12:44:01 +0600

    Update time_paste_on_cursor.py

--- .github/Projects/time_paste_on_cursor.py
+++ .github/Projects/time_paste_on_cursor.py
@@ -1,7 +1,18 @@
+"""
+A script that automatically types a certain sentence and pastes it at the cursor position every 2 seconds.
+Requires the 'pyautogui' library to be installed.
+"""
+
 import time
 import pyautogui
 
 def type_and_paste(sentence, interval):
+    """
+    Function to type a sentence and paste it at the cursor position.
+    Args:
+        sentence (str): The sentence to be typed and pasted.
+        interval (int): Time interval in seconds between each typing action.
+    """
     time.sleep(2)  # Wait for 2 seconds before starting
 
     while True:
@@ -13,8 +24,8 @@ def type_and_paste(sentence, interval):
         time.sleep(interval)  # Wait for the specified interval before typing again
 
 # Change the values below to customize the script
-sentence_to_type = "keo aso?"
-typing_interval = 2  # seconds
+SENTENCE_TO_TYPE = "Hello, World!"
+TYPING_INTERVAL = 2  # seconds
 
-type_and_paste(sentence_to_type, typing_interval)
+type_and_paste(SENTENCE_TO_TYPE, TYPING_INTERVAL)
     
