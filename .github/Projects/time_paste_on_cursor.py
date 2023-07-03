a9dc56274051d7ae96c7cbbd638507c73f023971
Farhanrahman063 <123469558+Farhanrahman063@users.noreply.github.com>
2023-07-03 17:14:04 +0600

    Update time_paste_on_cursor.py

--- .github/Projects/time_paste_on_cursor.py
+++ .github/Projects/time_paste_on_cursor.py
@@ -1,28 +1,26 @@
-"""
-A script that automatically types a certain sentence 
-and pastes it at the cursor position every 2 seconds.
-Requires the 'pyautogui' library to be installed.
-"""
-
 import time
 import pyautogui
 
 def type_and_paste(sentence, interval):
     """
-    Function to type a sentence and paste it at the cursor position.
+    Automatically types a sentence and pastes it at the cursor position.
     Args:
         sentence (str): The sentence to be typed and pasted.
         interval (int): Time interval in seconds between each typing action.
     """
     time.sleep(2)  # Wait for 2 seconds before starting
 
-    while True:
-        current_pos = pyautogui.position()  # Get the current cursor position
-        pyautogui.typewrite(sentence)  # Type the sentence
-        pyautogui.hotkey('ctrl', 'v')  # Paste the sentence at the cursor position
-        pyautogui.press('enter')  # Press Enter to simulate submitting the sentence
-        pyautogui.moveTo(current_pos)  # Move the cursor back to its original position
-        time.sleep(interval)  # Wait for the specified interval before typing again
+    try:
+        while True:
+            current_pos = pyautogui.position()  # Get the current cursor position
+            pyautogui.typewrite(sentence)  # Type the sentence
+            pyautogui.hotkey('ctrl', 'v')  # Paste the sentence at the cursor position
+            pyautogui.press('enter')  # Press Enter to simulate submitting the sentence
+            pyautogui.moveTo(current_pos)  # Move the cursor back to its original position
+            time.sleep(interval)  # Wait for the specified interval before typing again
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
 # Change the values below to customize the script
 SENTENCE_TO_TYPE = "Hello, World!"
@@ -30,3 +28,4 @@ TYPING_INTERVAL = 2  # seconds
 
 if __name__ == "__main__":
     type_and_paste(SENTENCE_TO_TYPE, TYPING_INTERVAL)
+    
